e87920ad95bb070bc061ee77db8eac81b44a72a9
John Otander <johnotander@gmail.com>
2017-11-06 08:38:52 -0700

    Pin latest class repeat, update tests

--- test/test.js
+++ test/test.js
@@ -5,17 +5,18 @@ import classRepeat from '../'
 
 test('postcss-class-repeat', t => {
   t.plan(2)
-  testFixture(t, 'input.css', 'output.css')
-  testFixture(t, 'input.css', 'output-repeat-multiple.css', { repeat: 4 })
+
+  testFixture(t, 'input.css')
+  testFixture(t, 'input.css', { repeat: 4 })
 })
 
 function fixture (name) {
-  return fs.readFileSync('fixtures/' + name, 'utf8')
+  return fs.readFileSync('./test/fixtures/' + name, 'utf8')
 }
 
 function testFixture (t, input, output, opts) {
   const result = postcss([ classRepeat(opts) ])
       .process(fixture(input))
 
-  t.same(result.css, fixture(output))
+  t.snapshot(result.css)
 }
